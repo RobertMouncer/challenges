17df1d6dd911ec367ab22fd7ed427bb79ad2824c
Robert Mouncer <robertmouncer@gmail.com>
2018-12-04 12:30:20 +0000

    model change to match swagger

--- challenges/Models/Challenge.cs
+++ challenges/Models/Challenge.cs
@@ -16,10 +16,14 @@ namespace challenges.Models
         public virtual DateTime EndDateTime { get; set; }
         [Required]
         public virtual int Goal { get; set; }
+        public virtual int PercentageComplete { get; set; }
         [Required]
         public virtual bool Repeat { get; set; }
         [Required]
         public virtual Activity Activity { get; set; }
+        public virtual int ActivityId { get; set; }
+        public virtual UserGroup UserGroup { get; set; }
+        public virtual int UserGroupId { get; set; }
 
 }
 }
---
+++ challenges/Models/UserGroup.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace challenges.Models
+{
+    public class UserGroup
+    {
+        [Required]
+        public virtual int UserGroupId { get; set; }
+
+        public virtual int UserId { get; set; }
+
+        public virtual int GroupId { get; set; }
+        public virtual bool isGroup{ get; set; }
+    }
+}
