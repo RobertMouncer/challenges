08d684a48b3e023590e967edcad237cf4b939d8c
Robert David Mouncer [rdm10] <rdm10@aber.ac.uk>
2018-12-11 16:36:09 +0000

    goal metric repository added. goal metric controller uses the repo. change to the challenges api.

--- challenges/Repositories/IGoalMetricRepository.cs
+++ challenges/Repositories/IGoalMetricRepository.cs
@@ -9,5 +9,10 @@ namespace challenges.Repositories
     public interface IGoalMetricRepository
     {
         Task<List<GoalMetric>> GetAllAsync();
+        Task<GoalMetric> AddAsync(GoalMetric goalMetric);
+        Task<GoalMetric> FindByIdAsync(int? id);
+        Task<GoalMetric> UpdateAsync(GoalMetric goalMetric);
+        Task<GoalMetric> DeleteAsync(GoalMetric goalMetric);
+        bool GoalMetricExists(int id);
     }
 }
